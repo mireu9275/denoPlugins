0c5ba8dc65f0e774ce8c9c0b0b852087ecb7a006
Kimhojung <mireu9275@naver.com>
2022-08-22 21:09:53 +0900

    moneyplugin

---
+++ src/main/java/deno/plugins/Plugins.java
@@ -0,0 +1,17 @@
+package deno.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
